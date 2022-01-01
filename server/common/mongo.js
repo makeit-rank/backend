96bb1618e964dba9a04a3443a26e1efe75404ebd
Raj <varasaniraj5@gmail.com>
2022-01-01 11:00:40 +0530

    Updated mongoos connection

--- server/common/mongo.js
+++ server/common/mongo.js
@@ -1,12 +1,17 @@
-import { MongoClient } from "mongodb";
+const mongoose = require("mongoose");
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
 
-// connect to mongodb
-export default async function connects() {
-    const client = await  MongoClient.connect(
-        process.env.MONGODB_URI,
-        { useNewUrlParser: true }
-    );
-    const db = client.db('test');
-    return { db, client };
-}
-    
+const model = mongoose.Schema({
+  name: String,
+  email: String,
+  mobile: Number,
+  password: String,
+});
+
+export const User = mongoose.model("User", model);
--- server/common/server.js
+++ server/common/server.js
@@ -1,18 +1,15 @@
-import Express from 'express';
+import Express from "express";
 
+import cors from "cors";
 
-import cors from 'cors';
-
-import * as path from 'path';
-import * as bodyParser from 'body-parser';
-import * as http from 'http';
-import * as os from 'os';
-import l from './logger';
-import oas from './swagger';
-
+import * as path from "path";
+import * as bodyParser from "body-parser";
+import * as http from "http";
+import * as os from "os";
+import l from "./logger";
+import oas from "./swagger";
 
 import mongo from "./mongo";
-
 
 const app = new Express();
 
@@ -20,43 +17,40 @@ export default class ExpressServer {
   constructor() {
     const root = path.normalize(`${__dirname}/../..`);
 
-    app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '100kb' }));
-    app.use(bodyParser.urlencoded({ extended: true, limit: process.env.REQUEST_LIMIT || '100kb' }));
-    app.use(bodyParser.text({ limit: process.env.REQUEST_LIMIT || '100kb'}));
+    app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || "100kb" }));
+    app.use(
+      bodyParser.urlencoded({
+        extended: true,
+        limit: process.env.REQUEST_LIMIT || "100kb",
+      })
+    );
+    app.use(bodyParser.text({ limit: process.env.REQUEST_LIMIT || "100kb" }));
 
     app.use(Express.static(`${root}/public`));
 
-
     app.use(cors());
-
   }
 
   router(routes) {
-
     this.routes = routes;
     return this;
-
   }
 
   listen(port = process.env.PORT) {
-    const welcome = p => () =>
+    const welcome = (p) => () =>
       l.info(
-        `up and running in ${process.env.NODE_ENV ||
-          'development'} @: ${os.hostname()} on port: ${p}}`
+        `up and running in ${
+          process.env.NODE_ENV || "development"
+        } @: ${os.hostname()} on port: ${p}}`
       );
 
     oas(app, this.routes)
       .then(() => {
-
-        mongo().then(() => {
-          l.info("Database Loaded!");
-          http.createServer(app).listen(port, welcome(port));
-        });
-
+        l.info("Database Loaded!");
+        http.createServer(app).listen(port, welcome(port));
       })
       .catch((e) => {
         l.error(e);
-        exit(1);
       });
 
     return app;
