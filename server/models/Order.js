e77d159a716822e52ac48d7e3bd881a466e7bd0a
WeryZebra-Yue <75676675+WeryZebra-Yue@users.noreply.github.com>
2022-01-12 22:50:36 +0530

    Name change in model and api

--- server/models/Order.js
+++ server/models/Order.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 const order = mongoose.Schema({
   product_id: String,
   size: String,
-  AttachedFiles: Array,
+  attachedFiles: Array,
   user_id: String,
   status: Object,
   created_at: Number,
   product_details: Object,
   seller_details: Object,
-  Address: Object,
+  address: Object,
 });
 
 export default new mongoose.model("Order", order);
