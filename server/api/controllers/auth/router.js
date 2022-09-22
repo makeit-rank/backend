53354ce23a0e05158d95ae306149b59f97be91a0
Raj <varasaniraj5@gmail.com>
2022-09-22 20:39:24 +0530

    updated test

--- server/api/controllers/auth/router.js
+++ server/api/controllers/auth/router.js
@@ -1,15 +1,13 @@
 import * as express from "express";
-import makeRapydRequest from "../../services/helpers/rapydAPI.service";
 import controller from "./controller";
 
 export default express
   .Router()
   .post("/login", controller.login)
   .get("/test", async (req, res) => {
+    // return origin name and address
     res.send({
-      host: req.get("host"),
-      originalUrl: req.originalUrl,
-      origin: req.get("origin"),
+      host: req,
     });
   })
   .post("/signup", controller.signup);
