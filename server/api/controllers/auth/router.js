1a5cfc8b456a802e68d1d66cd0d10af6c8bf7213
Raj <varasaniraj5@gmail.com>
2022-09-22 19:39:59 +0530

    Removed garbage code

--- server/api/controllers/auth/router.js
+++ server/api/controllers/auth/router.js
@@ -7,26 +7,6 @@ export default express
 
   .post("/login", controller.login)
   .get("/shit", async (req, res) => {
-    try {
-      const body = {
-        amount: 100,
-        complete_checkout_url: "http://example.com/complete",
-        country: "IN",
-        currency: "INR",
-        requested_currency: "INR",
-        merchant_reference_id: "950ae8c6-76",
-        // payment_method_types_include: [
-        //   "sg_credit_mastercard_card",
-        //   "sg_credit_visa_card",
-        // ],
-      };
-      const result = await makeRapydRequest("POST", "/v1/checkout", body);
-
-      console.log(result);
-      res.status(200).json(result);
-    } catch (error) {
-      console.error("Error completing request", error);
-      res.status(200).json(error);
-    }
+    res.send(req.get("host"));
   })
   .post("/signup", controller.signup);
